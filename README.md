0559448ab049c415f53b5e2fa34e731f4c0eb43d
Егор <e.kuzhal@itach.by>
2023-04-10 16:19:06 +0300

    new file

--- README.md
+++
@@ -1,2 +0,0 @@
-# projectPet
-pet-project
---
+++ const.js
@@ -0,0 +1,16 @@
+const contacts = [
+  { id: 1, title: '101' },
+  { id: 2, title: '102' },
+  { id: 3, title: '103' },
+  { id: 4, title: '104' },
+  { id: 5, title: '105' },
+]
+
+const data = [
+  { id: 1, title: 'Зарегистрирован' },
+  { id: 2, title: 'Активирован' },
+  { id: 3, title: 'Подключен' },
+  { id: 4, title: 'Ограничен' },
+  { id: 5, title: 'Зарезервирован' },
+]
+module.exports = { contacts, data }
